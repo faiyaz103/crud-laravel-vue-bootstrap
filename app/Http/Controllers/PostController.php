3c4beedabc373cde20760f7f5851853a4cf3d07b
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2025-04-28 23:36:51 +0600

    API Completed

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return Post::paginate(10);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $validated = $request->validate([
+            'title'=>'required|string|max:255',
+            'content'=>'required|string'
+        ]);
+
+        $post = Post::create($validated);
+
+        return response()->json($post, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        //
+        return $post;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+        $validated = $request->validate([
+            'title'=>'required|string|max:255',
+            'content'=>'required|string'
+        ]);
+
+        $post->update($validated);
+
+        return response()->json($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        //
+        $post->delete();
+
+        return response()->noContent();
+    }
+}
