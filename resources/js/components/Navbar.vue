b84e8b7f81dd3e9b3fd47e9c84e893a2ed3cdd12
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2025-04-29 12:34:07 +0600

    CSS Edited

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,34 +1,19 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 sticky-top">
-        <div class="container">
+    <nav class="navbar navbar-expand bg-dark navbar-dark py-3 sticky-top">
+      <div class="container">
+  
+        <a href="#" class="navbar-brand">CRUD</a>
 
-            <!-- Logo -->
-            <a href="#" class="navbar-brand">CRUD</a>
-
-            <!-- hamburger menu -->
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navmenu"
-            >
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <!-- for collapsing when screen is small than lg -->
-            <div class="collapse navbar-collapse" id="navmenu">
-                <ul class="navbar-nav">
-
-                    <li class="navbar-item">
-                        <router-link to="/" class="nav-link">Home</router-link>
-                    </li>
-                    <li class="navbar-item">
-                        <router-link to="/posts" class="nav-link">Posts</router-link>
-                    </li>
-
-                </ul>
-            </div>
-
-        </div>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/posts" class="nav-link">Posts</router-link>
+          </li>
+        </ul>
+  
+      </div>
     </nav>
-</template>
+  </template>
+  
